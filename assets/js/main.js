2790894866ae8a4f5bb94a75ffd03caa603d816f
phuccdung <82568398+phuccdung@users.noreply.github.com>
2021-10-14 11:07:31 +0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,54 @@ const skillsContent= document.getElementsByClassName('skills__content'),
  skillsHeader.forEach((el) => {
      el.addEventListener('click',toggleSkills)
  })
+
+ const tabs = document.querySelectorAll('[data-target]'),
+        tabContents = document.querySelectorAll('[data-content]')
+
+        tabs.forEach(tab=>{
+            tab.addEventListener('click',()=>{
+                const target= document.querySelector(tab.dataset.target)
+
+                tabContents.forEach(tabContent=>{
+                    tabContent.classList.remove('qualification__active')
+                })
+                target.classList.add('qualification__active')
+                tab.forEach(tab=>{
+                    tab.classList.remove('qualification__active')
+                })
+                tab.classList.add('qualification__active')
+            })
+        })
+
+    const modalViews=document.querySelectorAll('.services__modal'),
+        modalBtns=document.querySelectorAll('.services__button'),
+        modalClose=document.querySelectorAll('.services__modal-close')
+        
+        let modal =function(modalClick){
+            modalViews[modalClick].classList.add('active-modal')
+        }
+        modalBtns.forEach((modalBtn,i)=>{
+            modalBtn.addEventListener('click',()=>{
+                modal(i)
+            })
+        })
+        modalClose.forEach((modalClose)=>{
+            modalClose.addEventListener('click',()=>{
+                modalViews.forEach((modalView)=>{
+                    modalView.classList.remove('active-modal')
+                })
+            })
+        })
+
+        let swiper = new Swiper(".portfolio__container", {
+            cssMode: true,
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+              el: ".swiper-pagination",
+            },
+            mousewheel: true,
+            keyboard: true,
+          });
